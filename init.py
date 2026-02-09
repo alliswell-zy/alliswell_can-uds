d4356f73dce1427d56ae3969ac4c500ee43e1df3
alliswell-zy <alliswell_zhang@163.com>
2026-02-09 17:31:03 +0800

    Add files via upload
    utils

--- init.py
+++ init.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-用户界面模块 - 包含应用程序的所有用户界面组件
+工具模块 - 包含各种辅助函数、常量和验证器
 """
 
-__all__ = [
-    'main_window',
-    'can_setting_dialog',
-    'uds_session_widget',
-    'command_project_widget',
-    'monitor_widget',
-    'styles'
-]
+__all__ = ['constants', 'helpers', 'validators']
 
 # 版本信息
 __version__ = '1.0.0'
 __author__ = 'UDS Diagnostics Team'
-__description__ = 'UDS诊断工具用户界面模块'
+__description__ = 'UDS诊断工具工具模块'
